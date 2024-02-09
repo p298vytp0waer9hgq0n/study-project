6e8179c662aea2de301d10bcadf56d742e0fb4e8
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2024-02-10 01:31:32 +0500

    signup field validation

--- src/pages/authorization/sign-up.tsx
+++ src/pages/authorization/sign-up.tsx
@@ -12,17 +12,31 @@ type Inputs = {
 export function SignUp() {
     const { signUp } = useAuth();
 
-    const { register, handleSubmit } = useForm<Inputs>();
+    const {
+        register,
+        handleSubmit,
+        formState: { isDirty, isValid, errors },
+    } = useForm<Inputs>({ mode: 'all' });
 
     const onSubmit: SubmitHandler<Inputs> = (data) => signUp(data);
 
     return (
         <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
             <label>Email</label>
-            <input type="email" {...register('email', { required: true })} />
+            <input
+                type="text"
+                {...register('email', {
+                    required: true,
+                    pattern: { value: /[\w.-]+@[\w.-]+\.[\w.-]{2,}/gi, message: 'Incorrect email' },
+                })}
+            />
+            <span>{errors?.email?.message || ''}</span>
             <label>Password</label>
-            <input type="password" {...register('password', { required: true })} />
-            <button type="submit">Submit</button>
+            <input type="password" {...register('password', { required: 'Password is required' })} />
+            <span>{errors?.password?.message || ''}</span>
+            <button type="submit" disabled={!isDirty || !isValid}>
+                Submit
+            </button>
         </form>
     );
 }
